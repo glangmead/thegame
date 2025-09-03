b9420d1222793b45b361ebd165d6fb892b93f66b
Greg Langmead <glangmead@gmail.com>
2025-09-03 13:56:08 -0400

    tic-tac-toe now implemented

--- DynamicalSystems/Sources/ContentView.swift
+++ DynamicalSystems/Sources/ContentView.swift
@@ -84,6 +84,7 @@ struct Lens<A, B, C, D> {
 }
 
 typealias StateLens<A, C, D> = Lens<A, A, C, D>
+typealias OptionalStateLens<A, C, D> = Lens<A?, A, C, D>
 
 // Lens composition
 func <=> <Am, Ap, Bm, Bp, Cm, Cp>(
@@ -121,9 +122,19 @@ func ⊗ <Am, Ap, Bm, Bp, Cm, Cp, Dm, Dp>(
   )
 }
 
-// Tic Tac Toe
+//
+// MARK: Tic Tac Toe
+//
+
+
 enum TTTPlayer {
   case x, o
+  var inverted: TTTPlayer {
+    switch self {
+    case .x: return .o
+    case .o: return .x
+    }
+  }
 }
 
 enum TTTMark: Character, Identifiable {
@@ -131,44 +142,136 @@ enum TTTMark: Character, Identifiable {
   case x = "x"
   case o = "o"
   var id: Self { self }
+  var inverted: TTTMark {
+    switch self {
+    case .none: return .none
+    case .x: return .o
+    case .o: return .x
+    }
+  }
 }
 
 struct TTTState {
-  var board: [[TTTMark]] = [[.none, .none, .none],[.none, .none, .none],[.none, .none, .none]]
-  var lastMark: TTTMark = .none // the player who put the most recent mark shown on the board
+  let boardSize: Int
+  var board: [[TTTMark]]
+  var lastMark: TTTMark = .o // the player who put the most recent mark shown on the board
+  
+  init(_ boardSize: Int) {
+    self.boardSize = boardSize
+    self.board = [[TTTMark]](
+      repeating: [TTTMark](
+        repeating:.none,
+        count: boardSize),
+      count: boardSize)
+  }
+  var lines: [[(Int, Int)]] {
+    var lines: [[(Int, Int)]] = []
+    let vec = Array(0..<self.boardSize)
+    for i in 0..<self.boardSize {
+      lines.append(vec.map{ ($0, i)})
+      lines.append(vec.map{ (i, $0)})
+      lines.append(vec.map{ ($0, $0)})
+      lines.append(vec.map{ (-1 + self.boardSize - $0, $0)})
+    }
+    return lines
+  }
+  
+  var terminal: Bool {
+    return self.winner != nil
+  }
   
   var winner: TTTPlayer? {
-    if lastMark == .none { return nil }
-    return nil // TODO: look for three in a row of lastMark
-  }
-}
-
-struct TicTacToeView: View {
-  @State private var state = TTTState()
-  var body: some View {
-    VStack {
-      HStack {
-        ForEach(state.board[0]) {mark in
-          spaceView(mark)
-        }
-      }
-      HStack {
-        ForEach(state.board[1]) {mark in
-          spaceView(mark)
-        }
-      }
-      HStack {
-        ForEach(state.board[2]) {mark in
-          spaceView(mark)
+    for line in self.lines {
+      let boardLine = line.map{ self.board[$0.0][$0.1] }
+      for mark in [TTTMark.x, TTTMark.o] {
+        if boardLine.allSatisfy({$0 == mark}) {
+          return mark == .x ? .x : .o
         }
       }
     }
+    return nil
+  }
+}
+
+enum TTTAction {
+  case placeMark(Int, Int, TTTMark)
+}
+
+/// This is the lens for making a move. up() returns a nil state if it's illegal.
+let tttLens = OptionalStateLens<TTTState, TTTAction, TTTState>(
+  down: { $0 },
+  up: { (state, action) in
+    switch action {
+    case let .placeMark(row, col, mark):
+      if state.board[row][col] == .none {
+        var outState = state
+        outState.board[row][col] = mark
+        outState.lastMark = mark
+        return outState
+      } else {
+        return nil
+      }
+    }
+  }
+)
+
+/// This lens only supplies a meaningful upstream map, which returns all legal actions.
+let tttPossibleLegalLens = Lens<[TTTAction], TTTState, Void, Void>(
+  down: { s in return },
+  up: { (state, _) in
+    var legalActions: [TTTAction] = []
+    let rows = 0..<state.boardSize
+    let cols = 0..<state.boardSize
+    for row in rows {
+      for col in cols {
+        if state.board[row][col] == .none {
+          legalActions.append(.placeMark(row, col, state.lastMark.inverted))
+        }
+      }
+    }
+    return legalActions
+  }
+)
+
+struct TicTacToeView: View {
+  @State private var gameState = TTTState(3)
+  var body: some View {
+    let gameSizeRange = 0..<gameState.boardSize
+    VStack {
+      Spacer()
+      Text("Turn: \(gameState.lastMark.inverted)")
+        .font(.largeTitle)
+      Spacer()
+      ForEach(gameSizeRange, id: \.self) { rowNum in
+        HStack {
+          ForEach(gameSizeRange, id: \.self) { colNum in
+            spaceView(rowNum, colNum)
+          }
+        }
+      }
+      Spacer()
+      if gameState.terminal {
+        Text("\(gameState.winner!) wins!")
+          .font(.largeTitle)
+        Button("Play again") {gameState = TTTState(3)}.buttonStyle(.borderedProminent).font(.largeTitle)
+      }
+      Spacer()
+      Spacer()
+    }
   }
   
-  @ViewBuilder func spaceView(_ mark: TTTMark) -> some View {
+  @ViewBuilder func spaceView(_ row: Int, _ col: Int) -> some View {
+    let mark = gameState.board[row][col]
     switch mark {
     case .none:
-      Button("–") { }
+      Button("\(mark.rawValue) ") {
+        if !gameState.terminal {
+          let tttAction = TTTAction.placeMark(row, col, gameState.lastMark == .x ? .o : .x)
+          if let newState = tttLens.up((gameState, tttAction)) {
+            self.gameState = newState
+          }
+        }
+      }
       .font(.largeTitle)
     default:
       Text("\(mark.rawValue) ").font(.largeTitle)
